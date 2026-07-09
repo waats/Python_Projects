61a3a8654e4efc056a7644bb2cb57d31eb69fd6a
waats <98875983+waats@users.noreply.github.com>
2026-07-09 15:21:33 +0200

    decision making app.
    App to decide if the weather permits going outside.
    
    Utilizes if, else and elif statements

---
+++ temperature.py
@@ -0,0 +1,9 @@
+temperature = 75
+if temperature > 80:
+    print("It's too hot!")
+    print("Stay Inside!")
+elif temperature < 60:
+    print("It's too cold!")
+    print("Stay Inside!")
+else:
+    print("Enjoy the outdoors!")
